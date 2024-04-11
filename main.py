4283016da4c32d3b54c25a0f5303165fb0e42b92
AK16bit <akpainkiller1090999@gmail.com>
2024-04-11 21:58:22 +0800

    从 __name__ == "__main__" 抽离出 main() (a:24.102)

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from time import sleep, perf_counter
 
 from CS2 import CS2Process, isCS2ProcessExist, isCS2ProcessReady
-from utils import logger
+from utils import logger, ASCII_BASAGANNIDARR_MEDIUM, ASCII_AK32767_SMALL
 
-if __name__ == '__main__':
+
+def main() -> None:
     while not isCS2ProcessExist(): sleep(0.1)
     while not isCS2ProcessReady(): sleep(0.5)
 
@@ -19,3 +20,11 @@ if __name__ == '__main__':
 
     timeUse = (perf_counter() - timeUseCounter) * 1000
     logger.info("All Dump Finished in %s ms" % timeUse)
+
+
+if __name__ == '__main__':
+    print(ASCII_BASAGANNIDARR_MEDIUM)
+    print(ASCII_AK32767_SMALL)
+    print()
+
+    main()
