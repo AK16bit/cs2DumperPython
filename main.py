30bdb7625f844b888641bb8c9b974b0cbaa9b452
AK16bit <akpainkiller1090999@gmail.com>
2024-04-21 23:25:38 +0800

    默认禁用了convarDump (a:24.112)

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from os import mkdir
+from os import mkdir, system
 from os.path import exists
 from time import sleep, perf_counter
 
@@ -6,7 +6,7 @@ from CS2 import CS2Process, isCS2ProcessExist, isCS2ProcessReady
 from utils import logger, ASCII_BASAGANNIDARR_MEDIUM, ASCII_AK32767_SMALL
 
 
-def main(signature: bool = True, schema: bool = True, convar: bool = True) -> None:
+def main(signature: bool = True, schema: bool = True, convar: bool = False) -> None:
     while not isCS2ProcessExist(): sleep(0.1)
     while not isCS2ProcessReady(): sleep(0.5)
 
@@ -20,16 +20,19 @@ def main(signature: bool = True, schema: bool = True, convar: bool = True) -> No
         from Dumper.SchemaDump import dumpSchema
         dumpSchema()
     if convar:
+        # Actually, convarDump is meaningless, but I still coded it I dunno why :P
         from Dumper.ConvarDump import dumpConvar
         dumpConvar()
 
     timeUse = (perf_counter() - timeUseCounter) * 1000
     logger.info("All Dump Finished in %s ms" % timeUse)
 
+    system("pause.")
+
 
 if __name__ == '__main__':
     [logger.info(strLine) for strLine in ASCII_BASAGANNIDARR_MEDIUM.split("\n")]
     # [logger.info(strLine) for strLine in ASCII_AK32767_SMALL.split("\n")]
 
-    main(True, True, False)
+    main(True, True)
     
